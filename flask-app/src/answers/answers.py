be3374652f9352b55de2f45e8ccf1073c50d82e3
ConnerLusk <lusk.c@northeastern.com>
2023-04-17 20:33:34 -0400

    ahhh

--- flask-app/src/answers/answers.py
+++ flask-app/src/answers/answers.py
@@ -1,101 +1,55 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
-import json
-from src import db
-from utils import cursor_to_json
+from flask import Blueprint, request, request
+from utils import get_query, submit_query
 
 
 answers = Blueprint('answers', __name__)
 
 
-@answers.route('/answers/<gameId>', methods=['GET'])
+@answers.route('/answers/<gameId>', methods=['GET','DELETE'])
 def get_all_answers(gameId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to get all answers from the database
-    cursor.execute(f'SELECT * FROM Answers WHERE gameId = {gameId}')
-    
-    return cursor_to_json(cursor)
-
-
-@answers.route('/answers/<gameId>', methods=['DELETE'])
-def delete_game_answers(gameId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to delete all game answers
-    cursor.execute(f'DELETE * FROM Answers WHERE gameId = {gameId}')
-    
-    return "Deleted"
+    if request.method == 'GET':
+        return get_query(f'SELECT * FROM Answers WHERE gameId = {gameId}')
+    elif request.method == 'DELETE':
+        return submit_query(f'DELETE * FROM Answers WHERE gameId = {gameId}', "Deleted")
     
 
 
-@answers.route('/answers/<gameId>/<ValueRow>/<ValueColumn>', methods=['GET'])
-def get_answer(gameId, ValueRow, ValueColumn):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to query the database for an answer
-    cursor.execute(f'SELECT * FROM Answers WHERE gameId = {gameId} and ValueRow = {ValueRow} and {ValueColumn}')
+@answers.route('/answers/<gameId>/<ValueRow>/<ValueColumn>', methods=['GET','POST','PUT','DELETE'])
+def specific_answer(gameId, ValueRow, ValueColumn):
+    if request.method == 'GET':
+        return get_query(f'SELECT * FROM Answers WHERE gameId = {gameId} and ValueRow = {ValueRow} and {ValueColumn}')
     
-    return cursor_to_json(cursor)
+    elif request.method == 'POST':
+        the_data = request.json
 
+        gameId = the_data["gameId"]
+        gameName = the_data["gameName"]
+        ValueRow = the_data["valueRow"]
+        ValueColumn = the_data["valueColumn"]
+        charValue = the_data["charValue"]
 
-@answers.route('/answers/<gameId>/<ValueRow>/<ValueColumn>', methods=['POST'])
-def insert_answer(gameId, ValueRow, ValueColumn):
-
-    the_data = request.json
+        query = "INSERT INTO Answers (gameId, gameName, ValueRow, ValueColumn, charValue) VALUES ("
+        query += f"{gameId}, '{gameName}', {ValueRow}, {ValueColumn}, '{charValue}')"
+        
+        return submit_query(query, "Inserted")
     
-    current_app.logger.info(the_data)
+    elif request.method == 'PUT':
+        the_data = request.json
 
-    gameId = the_data["gameId"]
-    gameName = the_data["gameName"]
-    ValueRow = the_data["valueRow"]
-    ValueColumn = the_data["valueColumn"]
-    charValue = the_data["charValue"]
+        gameId = the_data["gameId"]
+        gameName = the_data["gameName"]
+        ValueRow = the_data["valueRow"]
+        ValueColumn = the_data["valueColumn"]
+        charValue = the_data["charValue"]
 
-    query = "INSERT INTO Answers (gameId, gameName, ValueRow, ValueColumn, charValue) VALUES ("
-    query += f"{gameId}, '{gameName}', {ValueRow}, {ValueColumn}, '{charValue}')"
+
+        # use cursor to update answers
+        query = f"UPDATE Answers SET gameName = '{gameName}'"
+        query += f", charValue = '{charValue}'"
+        query += f"WHERE gameId = {gameId} and valueRow = {ValueRow} and valueColumn = {ValueColumn}"
+
+        return submit_query(query, "Updated")
     
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-
-    return "Inserted"
-
-@answers.route('/answers/<gameId>/<ValueRow>/<ValueColumn>', methods=['PUT'])
-def update_answer(gameId, ValueRow, ValueColumn):
-    the_data = request.json
-    
-    current_app.logger.info(the_data)
-
-    gameId = the_data["gameId"]
-    gameName = the_data["gameName"]
-    ValueRow = the_data["valueRow"]
-    ValueColumn = the_data["valueColumn"]
-    charValue = the_data["charValue"]
-
-
-    # use cursor to update answers
-    query = f"UPDATE Answers SET gameName = '{gameName}'"
-    query += f", charValue = '{charValue}'"
-    query += f"WHERE gameId = {gameId} and valueRow = {ValueRow} and valueColumn = {ValueColumn}"
-
-    
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-
-    return "Updated"
-    
-@answers.route('/answers/<gameId>/<ValueRow>/<ValueColumn>', methods=['DELETE'])
-def delete_answer(gameId, gameName, ValueRow, ValueColumn, charValue):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    # use cursor to delete an answer
-    cursor.execute(f'DELETE * FROM Answers WHERE gameId = {gameId})')
-
-    return "Deleted"
+    elif request.method == 'DELETE':
+        query = f'DELETE * FROM Answers WHERE gameId = {gameId})'
+        return submit_query(query, "Deleted")
--- flask-app/src/gameAttempt/gameAttempt.py
+++ flask-app/src/gameAttempt/gameAttempt.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
-import json
+from flask import Blueprint, request, current_app, request
 from src import db
-from utils import cursor_to_json
+from utils import submit_query, get_query
 
 
 gameAttempt = Blueprint('gameAttempt', __name__)
@@ -15,65 +14,39 @@ def get_gameAttempts():
     # use cursor to query the database for a list of game attempts
     cursor.execute('SELECT * FROM GameAttempt')
     
-    return cursor_to_json(cursor)
+    return submit_query(cursor)
 
 # Get all the game attempts from the database
-@gameAttempt.route('/gameAttempt/<playerId>', methods=['GET'])
-def get_player_gameAttempts(playerId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
+@gameAttempt.route('/gameAttempt/<playerId>', methods=['GET','DELETE'])
+def player_id(playerId):
+    if request.method == 'GET':
+        return get_query(f'SELECT * FROM GameAttempt WHERE playerId = {playerId}')
+    elif request.method == 'DELETE':
+        return submit_query(f'DELETE FROM GameAttempt WHERE playerId = {playerId}', "Deleted")
 
-    # use cursor to query the database for a list of game attempts of the playerId
-    cursor.execute(f'SELECT * FROM GameAttempt WHERE playerId = {playerId}')
 
-    return cursor_to_json(cursor)
 
-@gameAttempt.route('/gameAttempt/<playerId>', methods=['DELETE'])
-def delete_player_gameAttempts(playerId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
 
-    cursor.execute(f'DELETE * FROM GameAttempt WHERE playerId = {playerId}')
-
-    return "Deleted"
-
-@gameAttempt.route('/gameAttempt/<playerId>/<gameId>', methods=['GET'])
+@gameAttempt.route('/gameAttempt/<playerId>/<gameId>', methods=['GET','POST','DELETE'])
 def get_game_gameAttempts(playerId, gameId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    cursor.execute(f'SELECT * FROM GameAttempt WHERE playerId = {playerId} and gameId = {gameId}')
-
-    return cursor_to_json(cursor)
-
-@gameAttempt.route('/gameAttempt/<playerId>/<gameId>', methods=['POST'])
-def post_game_gameAttempts(playerId, gameId):
-
-    the_data = request.json
+    if request.method == 'GET':
+        return get_query(f'SELECT * FROM GameAttempt WHERE playerId = {playerId} and gameId = {gameId}')
+    elif request.method == 'POST':
+        the_data = request.json
     
-    current_app.logger.info(the_data)
+        current_app.logger.info(the_data)
 
-    gameId = the_data["gameId"]
-    playerId = the_data["playerID"]
-    isInProgress = the_data["isInProgress"]
-    time_Elapsed = the_data["timeElapsed"]
-    score = the_data["score"]
+        gameId = the_data["gameId"]
+        playerId = the_data["playerID"]
+        isInProgress = the_data["isInProgress"]
+        time_Elapsed = the_data["timeElapsed"]
+        score = the_data["score"]
 
-    query = "INSERT INTO GameAttempt (gameId, playerID, isInProgress, timeElapsed, score) VALUES ("
-    query += f"{gameId}, {playerId}, {isInProgress}, {time_Elapsed}, {score})"
+        query = "INSERT INTO GameAttempt (gameId, playerID, isInProgress, timeElapsed, score) VALUES ("
+        query += f"{gameId}, {playerId}, {isInProgress}, {time_Elapsed}, {score})"
+
+        submit_query(query, "Inserted")
+
+    elif request.method == "DELETE":
+        return submit_query(f'DELETE * FROM GameAttempt WHERE playerId = {playerId} and gameId = {gameId}', "Deleted")
     
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
-
-    return "Inserted"
-
-@gameAttempt.route('/gameAttempt/<playerId>/<gameId>', methods=['DELETE'])
-def delete_game_gameAttempts(playerId, gameId):
-    # get a cursor object from the database
-    cursor = db.get_db().cursor()
-
-    cursor.execute(f'DELETE * FROM GameAttempt WHERE playerId = {playerId} and gameId = {gameId}')
-
-    return "Deleted"
