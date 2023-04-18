3aae47d04027b0240bc70b1742ad09e8c902d973
ConnerLusk <lusk.c@northeastern.com>
2023-04-17 20:13:24 -0400

    gametype done

--- flask-app/src/gameTypes/gameTypes.py
+++ flask-app/src/gameTypes/gameTypes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
-from utils import cursor_to_json
+from flask import Blueprint
+from utils import get_query
 
 
 gameType = Blueprint('gameType', __name__)
@@ -7,4 +7,4 @@ gameType = Blueprint('gameType', __name__)
 # Get all the game attempts from the database
 @gameType.route('/', methods=['GET'])
 def game_types():
-    return cursor_to_json("SELECT * FROM GameType")
+    return get_query("SELECT * FROM GameType")
