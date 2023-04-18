b5ab797f7d6ecde65f26e57e41374ed1c4f43e60
Gus Mueller <gusmueller@Guss-MacBook-Pro.local>
2023-04-18 17:38:35 -0400

    adding put route for submission

--- flask-app/src/submission/submission.py
+++ flask-app/src/submission/submission.py
@@ -1,9 +1,11 @@
-from flask import Blueprint
-from utils import get_query
+from flask import Blueprint, request, request
+from utils import get_query, submit_query
 
 submission = Blueprint('submission', __name__)
 
 # Get all the submissions by a player
+
+
 @submission.route('/submission/<playerId>', methods=['GET'])
 def get_player_submission(playerId):
     query = f'''
@@ -14,6 +16,8 @@ def get_player_submission(playerId):
     return get_query(query)
 
 # Get all the submissions by a player for a specific game
+
+
 @submission.route('/submission/<playerId>/<gameId>', methods=['GET'])
 def get_player_game_submission(playerId, gameId):
     query = f'''
@@ -23,7 +27,9 @@ def get_player_game_submission(playerId, gameId):
     '''
     return get_query(query)
 
-# # Return a specific submission given a submission number 
+# # Return a specific submission given a submission number
+
+
 @submission.route('/submission/<playerId>/<gameId>/<submissionNumber>', methods=['GET'])
 def get_player_game_submission_num(playerId, gameId, submissionNumber):
     query = f'''
@@ -33,3 +39,15 @@ def get_player_game_submission_num(playerId, gameId, submissionNumber):
     '''
     return get_query(query)
 
+
+@submission.route('/submission/<submissionNumber>/<attemptId>', methods=['PUT'])
+def put_player_game_submission_num(submissionNumber, attemptId):
+    the_data = request.json
+    submissionNumber = the_data["submissonNumber"]
+    attemptId = the_data["attemptId"]
+    numIncorrect = 3
+
+    query = f"UPDATE Submission SET numIncorrect = '{numIncorrect}' WHERE attemptId = {attemptId} and\
+            submissionNumber = {submissionNumber};"
+
+    return submit_query(query, "Updated")
