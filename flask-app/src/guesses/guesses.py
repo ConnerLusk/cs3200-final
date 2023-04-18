371abe818f05bb3e4f0386bfc0284d12b02d4820
ConnerLusk <lusk.c@northeastern.com>
2023-04-17 20:10:21 -0400

    Guesses Done

--- flask-app/src/guesses/guesses.py
+++ flask-app/src/guesses/guesses.py
@@ -1,28 +1,24 @@
 from flask import Blueprint, request
-from utils import cursor_to_json, submit_query
+from utils import get_query, submit_query
 
 
 guesses = Blueprint('guesses', __name__)
 
-@guesses.route('/', methods=['GET'])
-def guessess():
-    return cursor_to_json("SELECT * FROM Guesses")
-
 
 @guesses.route('/guesses/<attemptId>', methods=['GET'])
 def guesses_for_attempt(attemptId):
-    return cursor_to_json(f"SELECT * FROM Guesses WHERE attemptId = {attemptId};")
+    return get_query(f"SELECT * FROM Guesses WHERE attemptId = {attemptId};")
 
 @guesses.route('/guesses/<attemptId>/<submissionNumber>/<row>/<col>', methods=['GET','POST','PUT','DELETE'])
 def guesses_for_attempt_specific(attemptId, submissionNumber, row, col):
     if request.method == 'GET':
-        return cursor_to_json(f"SELECT * FROM Guesses WHERE attemptId = {attemptId} and valueRow = {row} and\
+        return get_query(f"SELECT * FROM Guesses WHERE attemptId = {attemptId} and valueRow = {row} and\
                               valueColumn = {col} and submissionNumber = {submissionNumber};")
     elif request.method == 'POST':
         data = request.json
-        query = f"INSERT INTO Guesses (submissionNumber, attemptId, valueRow, valueColumn, charValue) VALUES \
+        query = f"INSERT INTO Guesses (submissionNumber, attemptId, valueRow, valueColumn, charValue) VALUES\
             ({submissionNumber}, {attemptId}, {row}, {col}, '{data['charValue']}');"
-        return submit_query(query, "Updated")
+        return submit_query(query, "Inserted")
     elif request.method == 'PUT':
         data = request.json
         query = f"UPDATE Guesses SET charValue = '{data['charValue']}' WHERE attemptId = {attemptId} and valueRow = {row} and\
@@ -32,4 +28,3 @@ def guesses_for_attempt_specific(attemptId, submissionNumber, row, col):
         query = f"DELETE FROM Guesses WHERE attemptId = {attemptId} and valueRow = {row} and\
             valueColumn = {col} and submissionNumber = {submissionNumber};"
         return submit_query(query, "Deleted")
-    
